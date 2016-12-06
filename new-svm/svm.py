b0942ad8eb8ddef5c6bbbb3151667707ba714416
katgorski <kat.gorski@gmail.com>
2016-12-06 00:33:22 -0500

    svm

---
+++ new-svm/svm.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python2
+
+from __future__ import print_function
+import numpy as np
+import funct
+from sys import argv
+from sklearn import svm
+
+script, d_f, l_f, filename = argv
+
+data = funct.Data(d_f, l_f, w0 = False)
+labels = data.test_labels
+training_data = data.training_rows
+
+training_data = [data.data[i] for i in training_data]
+training_labels = [data.labels[i] for i in data.training_rows]
+
+test_data = [data.data[i] for i in labels]
+
+print("\nWorking with %s, out file prefix %s" % (l_f, filename))
+data = np.asarray(training_data)
+
+svm_clf = svm.LinearSVC()
+svm_clf.fit(data, training_labels)
+predictions = svm_clf.predict(test_data)
+with open(filename + ".predict", "w") as f:
+    for i in range(len(labels)):
+        f.write("%d %d\n" % (predictions[i], labels[i]))
+print("Data shape overall: ", data.shape)
