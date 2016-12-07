1383c26debe06059d5574cbff57718485424c36b
v2rockets <zypcastle@hotmail.com>
2016-12-06 22:33:02 -0500

    fixed problem

--- Feature-Selection/mutual_information.py
+++ Feature-Selection/mutual_information.py
@@ -3,7 +3,6 @@ import math
 
 ### read data
 f=open(sys.argv[1]);
-boundry=100;
 
 data=[];
 i=0;
@@ -17,48 +16,52 @@ while(l!=''):
     l=f.readline();
     i+=1;
     print("read ",i);
-n=len(data);
+nd=len(data);
 m=len(data[0]);
 
 ### read train labels
 f=open(sys.argv[2]);
-label=[-1]*n;
+label=[-1]*nd;
 l=f.readline();
+n=0;
 while(l!=''):
     a=l.split();
     index=int(a[1]);
     label[index]=int(a[0]);
+    n+=1;
     l=f.readline();
     
 #Use mutual information to do feature selection
 
 mi=[0]*m;
 for j in range(0,m):
-    py=[0]*2;
-    px=[0]*3;
-    pyx=[];
-    pyx.append([0]*3);
-    pyx.append([0]*3);
+    fy=[0]*2;
+    fx=[0]*3;
+    fyx=[];
+    fyx.append([0]*3);
+    fyx.append([0]*3);
     
-    for i in range(0,n):
+    for i in range(0,nd):
         y=label[i];
+        if(y==-1):
+            continue;
         x=data[i][j];
-        px[x]+=1/n;
-        py[y]+=1/n;
-        pyx[y][x]+=1/n;
+        fx[x]+=1;
+        fy[y]+=1;
+        fyx[y][x]+=1;
     for y in range(0,2):
         for x in range(0,3):
-            if(pyx[y][x]>0):
-                t=pyx[y][x]/(px[x]*py[y]);
+            if(fyx[y][x]>0):
+                t=fyx[y][x]*n/(fx[x]*fy[y]);
                 if(t<=0):
                     continue;
-                mi[j]+=pyx[y][x]*math.log2(t);
+                mi[j]+=fyx[y][x]/n*math.log2(t);
     print(j," ",mi[j]);
 
 
 
 ### Prediction
-f1=open("mutual_infomation", "w+");
+f1=open("mutual_information", "w+");
 for j in range(0,m):
     f1.write(str(mi[j])+'\n');
 f1.close();
